a589514113c49ad5861b378ab8a051cb03860a2c
GuyGabriell <jonahgabriel85@yahoo.com>
2024-09-12 11:18:54 +0100

    build a better router

--- routes.php
+++ routes.php
@@ -1,10 +1,22 @@
 <?php
 
-return [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/notes/show.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/contact' => 'controllers/contact.php',
-];
+//return [
+//    
+//    '/' => 'controllers/index.php',
+//    '/about' => 'controllers/about.php',
+//    '/notes' => 'controllers/notes/index.php',
+//    '/note' => 'controllers/notes/show.php',
+//    '/notes/create' => 'controllers/notes/create.php',
+//    '/contact' => 'controllers/contact.php',
+//    
+//];
+
+
+$router->get('/', 'controller/index.php');
+$router->get('/about', 'controller/about.php');
+$router->get('/conatct', 'controller/contact.php');
+
+
+$router->get('/notes', 'controller/notes/index.php');
+$router->get('/note', 'controller/notes/show.php');
+$router->get('/notes/create', 'controller/notes/create.php');
